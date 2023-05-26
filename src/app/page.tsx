53414ebe947e24b970f48368bf07341175417d2b
paresiqbal <thecrowdel@gmail.com>
2023-05-26 23:42:47 +0700

    Adding idont know

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 async function getPost() {
-  const res = await fetch("/api/getPost");
+  const res = await fetch(`${process.env.BASE_URL}/api/getPost`);
+  if (!res.ok) {
+    console.log(res);
+  }
+
+  return res.json();
 }
 
-export default function Home() {
+export default async function Home() {
+  const data = await getPost();
+
+  console.log(data);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
