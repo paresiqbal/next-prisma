ba95ed12a1b311b2693b33dc102eaf0f9cb05710
paresiqbal <thecrowdel@gmail.com>
2023-05-26 23:26:30 +0700

    Fetching API in different components

---
+++ api/getPost.ts
@@ -0,0 +1,21 @@
+// next
+import type { NextApiRequest, NextApiResponse } from "next";
+
+// library
+import prisma from "../prisma/client";
+
+
+export default async function handler (
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === "GET") {
+        try {
+            // Get prisma to fetch the post
+            const data = await prisma.post.findMany()
+            return res.status(200).json(data)
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+async function getPost() {
+  const res = await fetch("/api/getPost");
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
